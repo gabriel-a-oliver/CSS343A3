a144f391440f5c10ab676a01ff07a0f11174340a
Gabriel O <olivergabe@hotmail.com>
2021-07-28 16:40:33 -0700

    created clion project

---
+++ assignment3/cmake-build-debug/CMakeFiles/assignment3.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "B:/HardDrive_Documents/School/University_of_Washington_Bothell/UWB_Classes/UWB_2020-2021/UWB_Summer_2021/CSS343/Assignments/Assignment3/CSS343A3/assignment3/assignment3.cpp" "B:/HardDrive_Documents/School/University_of_Washington_Bothell/UWB_Classes/UWB_2020-2021/UWB_Summer_2021/CSS343/Assignments/Assignment3/CSS343A3/assignment3/cmake-build-debug/CMakeFiles/assignment3.dir/assignment3.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ assignment3/cmake-build-debug/CMakeFiles/assignment3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment3.dir/assignment3.cpp.obj"
+  "assignment3.exe"
+  "assignment3.exe.manifest"
+  "assignment3.lib"
+  "assignment3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
