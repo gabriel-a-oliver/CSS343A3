08302e64be3da424587548f29c48f68f51e115a2
psinghal12 <46230624+psinghal12@users.noreply.github.com>
2021-07-29 21:01:29 -0700

    Update edge.cpp

--- assignment3/edge.cpp
+++ assignment3/edge.cpp
@@ -7,14 +7,21 @@
 // This is 80 characters - Keep all lines under 80 characters                 //
 ////////////////////////////////////////////////////////////////////////////////
 
-
+//the initializers in edge.h mean that we don't need to do anything in the default constr
 Edge::Edge() {}
 
 /** constructor with label and weight */
-Edge::Edge(const std::string& end, int weight) {}
+Edge::Edge(const std::string& end, int weight) {
+  this->endVertex = end;
+	this->edgeWeight = weight;
+}
 
 /** return the vertex this edge connects to */
-std::string Edge::getEndVertex() const { return "XXX"; }
+std::string Edge::getEndVertex() const { 
+  return this->endVertex; 
+}
 
 /** return the weight/cost of travlleing via this edge */
-int Edge::getWeight() const { return 0; }
+int Edge::getWeight() const { 
+  return this->edgeWeight; 
+}
