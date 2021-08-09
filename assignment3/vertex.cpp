fcb760506e349b7bf4f7a995251ecc2e2475c640
psinghal12 <46230624+psinghal12@users.noreply.github.com>
2021-08-08 21:26:07 -0700

    Update vertex.cpp

--- assignment3/vertex.cpp
+++ assignment3/vertex.cpp
@@ -21,33 +21,47 @@
 Vertex::Vertex(std::string label) {}
 
 /** @return  The label of this vertex. */
-std::string Vertex::getLabel() const { return ""; }
+string Vertex::getLabel() const { 
+    return this->vertexLabel; 
+}
 
 /** Marks this vertex as visited. */
-void Vertex::visit() {}
+void Vertex::visit() {
+    this->visited = true;
+}
 
 /** Marks this vertex as not visited. */
-void Vertex::unvisit() {}
+void Vertex::unvisit() {
+    this->visited = false;
+}
 
 /** Returns the visited status of this vertex.
  @return  True if the vertex has been visited, otherwise
     returns false/ */
-bool Vertex::isVisited() const { return true; }
+bool Vertex::isVisited() const { 
+    return (this->visited);
+}
 
 /** Adds an edge between this vertex and the given vertex.
     Cannot have multiple connections to the same endVertex
     Cannot connect back to itself
  @return  True if the connection is successful. */
-bool Vertex::connect(const std::string& endVertex, const int edgeWeight) { return true; }
+bool Vertex::connect(const string& endVertex, const int edgeWeight) { 
+    Edge e(endVertex, edgeWeight);
+    return false; //default initialization
+    //how to handle the connections to itself? connections to same vertex?
+}
 
 /** Removes the edge between this vertex and the given one.
 @return  True if the removal is successful. */
-bool Vertex::disconnect(const std::string& endVertex) {return true; }
+bool Vertex::disconnect(const std::string& endVertex) { return true; }
 
 /** Gets the weight of the edge between this vertex and the given vertex.
  @return  The edge weight. This value is zero for an unweighted graph and
     is negative if the .edge does not exist */
-int Vertex::getEdgeWeight(const std::string& endVertex) const { return 0; }
+int Vertex::getEdgeWeight(const std::string& endVertex) const { 
+    
+}
 
 /** Calculates how many neighbors this vertex has.
  @return  The number of the vertex's neighbors. */
@@ -60,13 +74,21 @@ void Vertex::resetNeighbor() {}
     Neighbors are automatically sorted alphabetically via map
     Returns the vertex label if there are no more neighbors
  @return  The label of the vertex's next neighbor. */
-std::string Vertex::getNextNeighbor() {return "XXX"; }
+std::string Vertex::getNextNeighbor() { return "XXX"; }
 
 /** Sees whether this vertex is equal to another one.
     Two vertices are equal if they have the same label. */
-bool Vertex::operator==(const Vertex& rightHandItem) const { return true; }
+bool Vertex::operator==(const Vertex& rightHandItem) const { 
+    std::string lhsLabel = this->getLabel();
+    std::string rhsLabel = rightHandItem.getLabel();
+    return lhsLabel.compare(rhsLabel) == 0;
+}
 
 /** Sees whether this vertex is < another one.
     Compares vertexLabel. */
-bool Vertex::operator<(const Vertex& rightHandItem) const {return true; }
+bool Vertex::operator<(const Vertex& rightHandItem) const { 
+    std::string lhsLabel = this->getLabel();
+    std::string rhsLabel = rightHandItem.getLabel();
+    return lhsLabel.compare(rhsLabel) < 0;
+}
 
