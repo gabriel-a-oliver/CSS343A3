67beae1737cedf7d1e3f2665a02dbe7d76e03544
psinghal12 <46230624+psinghal12@users.noreply.github.com>
2021-08-12 11:42:42 -0700

    added some imps of newer methods

--- assignment3/vertex.cpp
+++ assignment3/vertex.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <climits>
 
 #include "vertex.h"
@@ -6,10 +5,14 @@
 
 #include <functional>
 #include <map>
+#include <iterator>
 #include <string>
+#include <set>
 
 #include "edge.h"
 using namespace std;
+
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // This is 80 characters - Keep all lines under 80 characters                 //
@@ -22,7 +25,7 @@ using namespace std;
 Vertex::Vertex(std::string label) {}
 
 /** @return  The label of this vertex. */
-std::string Vertex::getLabel() const {
+string Vertex::getLabel() const { 
     return this->vertexLabel; 
 }
 
@@ -49,7 +52,17 @@ bool Vertex::isVisited() const {
  @return  True if the connection is successful. */
 bool Vertex::connect(const string& endVertex, const int edgeWeight) { 
     Edge e(endVertex, edgeWeight);
-    return false; //default initialization
+    adjacencyList.emplace(this->vertexLabel, e);
+    //iterate w/iterator, check if this vertexlbl.second
+    //maybe put this up top?
+    map<std::string, Edge>::iterator it;
+    for (it = adjacencyList.begin(); it != adjacencyList.end(); it++) {
+        if (it->second.getEndVertex() == this->vertexLabel) {
+            //connection to self - like A->A
+            return false;
+        }
+    }
+    return true; //default initialization
     //how to handle the connections to itself? connections to same vertex?
 }
 
@@ -61,15 +74,39 @@ bool Vertex::disconnect(const std::string& endVertex) { return true; }
  @return  The edge weight. This value is zero for an unweighted graph and
     is negative if the .edge does not exist */
 int Vertex::getEdgeWeight(const std::string& endVertex) const { 
-    return -999;
+    //say this = D, end = F, get weight of 10
+    //itreate!
+    map<std::string, Edge>::iterator it;
+    for (it = adjacencyList.begin(); it != adjacencyList.end(); it++) {
+        if (it->second.getEndVertex() == endVertex) {
+            return it->second.getWeight();
+        }
+    }
 }
 
 /** Calculates how many neighbors this vertex has.
  @return  The number of the vertex's neighbors. */
-int Vertex::getNumberOfNeighbors() const { return 0; }
+int Vertex::getNumberOfNeighbors() const { 
+    //iterate thru list, create vector of neighbors
+    //everytime you see a element with this->vertexLabel as its starting vertex,
+    //pushback it's second.endVert into the vector
+    //then count the unique/distinct vertices
+    //OR, USE A SET!!!! DOESN'T ALLOW DUPES ANYWAY
+    set<string> distinctVerts;
+    map<std::string, Edge>::iterator it;
+    for (it = adjacencyList.begin(); it != adjacencyList.end(); it++) {
+        if (it->first.compare(this->vertexLabel)) {
+            distinctVerts.insert(it->second.getEndVertex()); //gathers up distinct neighbors
+        }
+    }
+
+    return distinctVerts.size();
+}
 
 /** Sets current neighbor to first in adjacency list. */
-void Vertex::resetNeighbor() {}
+void Vertex::resetNeighbor() {
+    //currNeighbor = adjlist.begin
+}
 
 /** Gets this vertex's next neighbor in the adjacency list.
     Neighbors are automatically sorted alphabetically via map
